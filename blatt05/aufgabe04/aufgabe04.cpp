0774746c8653539b0f82f63624d7baedb7be0239
Christian Rebischke <chris@nullday.de>
2019-05-20 23:20:52 +0200

    implemented aufgabe04.cpp

--- blatt05/aufgabe04/aufgabe04.cpp
+++ blatt05/aufgabe04/aufgabe04.cpp
@@ -5,6 +5,23 @@
 #include <string>
 #include <exception>
 
+class my_exception: public std::exception {
+public:
+
+    // Our constructor with move statement, to move ownership of the string to my_exception
+    explicit my_exception(std::string exception): exception(std::move(exception)){
+    }
+
+    // our watch function
+    std::string watch(){
+        return this->exception;
+    }
+
+private:
+    // we use 'watch' for returning the string, therefore it can be private
+    std::string exception;
+};
+
 //class Foo throws in the destructor
 class Foo {
 public:
